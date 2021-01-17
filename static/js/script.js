52b04ed80f49b0362555d9faf8c3454bb3a885a9
pacotools <juanfjuarez@kolibee.com>
2021-01-16 19:45:58 -0600

    added mobile header and navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
+    $('.tabs').tabs();
+    $('.modal').modal();
     $('.collapsible').collapsible();
     $('select').formSelect();
 });
