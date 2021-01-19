386f0b770391a51f4347c2e90f4943c629874c11
pacotools <juanfjuarez@kolibee.com>
2021-01-18 19:46:54 -0600

    added product details functionality

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
     $('.tabs').tabs();
+    $('.materialboxed').materialbox();
     $('.modal').modal();
     $('.collapsible').collapsible();
     $('select').formSelect();
